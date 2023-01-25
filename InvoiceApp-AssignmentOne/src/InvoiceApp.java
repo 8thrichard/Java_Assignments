dac6ec6c5c52f221082c6fac83c1c4567b1252af
Richard <118012295+8thrichard@users.noreply.github.com>
2023-01-25 11:04:24 -0600

    GUI Line Added for "Welcome to Invoice TC"

--- InvoiceApp-AssignmentOne/src/InvoiceApp.java
+++ InvoiceApp-AssignmentOne/src/InvoiceApp.java
@@ -1,5 +1,6 @@
-package InvoiceApp;
 /* Programmer: Richard Boamah | Date: 01/21/2023 | */
+package InvoiceApp;
+
 import java.util.Scanner;
 
 public class InvoiceApp {
@@ -7,7 +8,7 @@ public class InvoiceApp {
     public static void main(String[] args) {
         // welcome the user to the program
         System.out.println("Welcome to the Invoice Total Calculator");
-        System.out.println(); // print a blank line
+        System.out.println("========================================"); // print a blank line
 
         // create a Scanner object named sc
         Scanner sc = new Scanner(System.in);
@@ -63,4 +64,5 @@ public class InvoiceApp {
         System.out.println(message);
     }
 }
+
 // End of InvoiceApp
