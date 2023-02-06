1a5aebea3265280a6b8c16fdb1ee0911d976b477
Richard <118012295+8thrichard@users.noreply.github.com>
2023-02-06 10:45:26 -0600

    Init

--- BankApp-AssignmentTwo/src/App.java
+++ BankApp-AssignmentTwo/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to BankApp!");
+            Account richardAccount = new Account();
+            Account johnAccount = new Account(1000.10, "John", "Owen");
+
+
+            System.out.println(richardAccount.number);
+            System.out.println(johnAccount.number);
     }
 }
